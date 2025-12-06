f90ac7b8b6f59536d49d66368516cc973ded7c6b
CanMumcu <canmmc15@hotmail.com>
2025-12-06 15:37:10 +0300

    Login screen and UI components (#5)

--- app/src/main/java/com/yeditepe/seventalkapp/MainActivity.kt
+++ app/src/main/java/com/yeditepe/seventalkapp/MainActivity.kt
@@ -11,37 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.yeditepe.seventalkapp.screens.LoginScreen
 import com.yeditepe.seventalkapp.ui.theme._7TalkAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            _7TalkAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+            LoginScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _7TalkAppTheme {
-        Greeting("Android")
     }
 }
